5c0390b6ff0bb3c21d8f6a11d4e99bd3b350dbcc
Guillaume <g.roge@rubika-edu.com>
2021-04-02 14:30:43 +0200

    1er Test

---
+++ Tanko/Assets/Script/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    public Rigidbody2D playerRb;
+    public float horizontaleAxe;
+    public float verticalAxe;
+
+    void Update()
+    {
+        horizontaleAxe = Input.GetAxis("Horizontal");
+        verticalAxe = Input.GetAxis("Vertical");
+
+        if (horizontaleAxe >= 0.1f)
+        {
+            playerRb.velocity = new Vector2(horizontaleAxe, 0) * Time.deltaTime * speed;
+        }
+        
+        if (horizontaleAxe <= -0.1f)
+        {
+            playerRb.velocity = new Vector2(horizontaleAxe, 0) * Time.deltaTime * speed;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            playerRb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
