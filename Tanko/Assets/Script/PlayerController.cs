c05ed553a8d37a48c81c6928b953280566d43e2b
Guillaume <g.roge@rubika-edu.com>
2021-04-07 10:51:35 +0200

    Rotation système

--- Tanko/Assets/Script/PlayerController.cs
+++ Tanko/Assets/Script/PlayerController.cs
@@ -6,34 +6,74 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
+    public float rotationSpeed;
 
     [Header ("Unity Setup")]
     public Rigidbody2D playerRb;
-    public float horizontaleAxe;
-    public float verticalAxe;
+    public Transform jumpDirection;
+    public LayerMask groundLayer;
 
     [Header("Raycast info")]
+    public float rayDistance;
     public Transform leftStartRaycast;
     public Transform rightStartRaycast;
 
-    void Update()
+    [Header("Info")]
+    public bool inTheAir;
+    [SerializeField]
+    private float horizontaleAxe;
+    [SerializeField]
+    private float verticalAxe;
+    private RaycastHit2D hit;
+
+    void FixedUpdate()
     {
         horizontaleAxe = Input.GetAxis("Horizontal");
         verticalAxe = Input.GetAxis("Vertical");
 
-        if (horizontaleAxe >= 0.1f)
-        {
-            playerRb.AddForce(new Vector2(horizontaleAxe, 0) * Time.deltaTime * speed);
-        }
-        
-        if (horizontaleAxe <= -0.1f)
-        {
-            playerRb.AddForce(new Vector2(horizontaleAxe, 0) * Time.deltaTime * speed);
-        }
+        Movement();
+        Jump();
+        AirVerification();
+    }
 
-        if (Input.GetButtonDown("Jump"))
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && !inTheAir)
         {
-            playerRb.AddForce(Vector2.up * jumpForce);
+            Vector2 jumpDir = (jumpDirection.position - transform.position).normalized;
+            playerRb.AddForce(jumpDir * jumpForce);
+        }
+    }
+    void Movement()
+    {
+        if (!inTheAir)
+        {
+            playerRb.AddForce(new Vector2(horizontaleAxe, 0) * Time.deltaTime * speed);
+        }
+        else
+        {
+            if (horizontaleAxe > 0.1f)
+            {
+                playerRb.rotation -= rotationSpeed;
+            }
+            else if (horizontaleAxe < -0.1f)
+            {
+                playerRb.rotation += rotationSpeed;
+            }
+        }
+    }
+  
+    void AirVerification()
+    {
+        hit = Physics2D.Raycast(leftStartRaycast.position, leftStartRaycast.up, rayDistance, groundLayer);
+
+        if (hit.collider != null) // Si on touche
+        {
+            inTheAir = false;
+        }
+        else
+        {
+            inTheAir = true;
         }
     }
 }
